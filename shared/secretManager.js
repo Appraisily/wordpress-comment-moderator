20482d393b0064cbf2b271899c04002940156c26
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-10-31 12:24:14 +0100

    Update secretManager.js

--- shared/secretManager.js
+++ shared/secretManager.js
@@ -1,10 +1,8 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { config } = require('./config'); // Asegúrate de que la ruta es correcta
 
 const client = new SecretManagerServiceClient();
 
-async function getSecret(name) {
-  const projectId = config.GOOGLE_CLOUD_PROJECT_ID;
+async function getSecret(projectId, name) {
   const [version] = await client.accessSecretVersion({
     name: `projects/${projectId}/secrets/${name}/versions/latest`,
   });
