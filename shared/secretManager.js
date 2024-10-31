736b5b75b9ce2750ffe371fc3bc3256b051f9f63
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-10-31 13:55:19 +0100

    Update secretManager.js

--- shared/secretManager.js
+++ shared/secretManager.js
@@ -1,10 +1,18 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
+// Obtener el ID del proyecto desde las variables de entorno
+const projectId = process.env.GOOGLE_CLOUD_PROJECT;
+
+if (!projectId) {
+  throw new Error('La variable de entorno GOOGLE_CLOUD_PROJECT no está definida.');
+}
+
+// Inicializa el cliente de Secret Manager
 const client = new SecretManagerServiceClient();
 
-async function getSecret(projectId, name) {
+async function getSecret(secretName) {
   const [version] = await client.accessSecretVersion({
-    name: `projects/${projectId}/secrets/${name}/versions/latest`,
+    name: `projects/${projectId}/secrets/${secretName}/versions/latest`,
   });
 
   const payload = version.payload.data.toString('utf8');
