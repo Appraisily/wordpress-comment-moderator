8336cac59314e00154430618f4f1497158d88226
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-10-31 12:24:43 +0100

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,22 +1,29 @@
 const { getSecret } = require('./secretManager');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 const config = {};
 
 async function initializeConfig() {
   try {
-    config.WORDPRESS_USERNAME = (await getSecret('wp_username')).trim();
-    config.WORDPRESS_APP_PASSWORD = (await getSecret('wp_app_password')).trim();
-    config.WORDPRESS_API_URL = (await getSecret('WORDPRESS_API_URL')).trim();
+    // Inicializar el cliente de Secret Manager
+    const client = new SecretManagerServiceClient();
+    
+    // Obtener el Project ID automáticamente
+    const [projectId] = await client.getProjectId();
+    config.GOOGLE_CLOUD_PROJECT_ID = projectId;
+    console.log('GOOGLE_CLOUD_PROJECT_ID obtenido correctamente.');
+
+    // Obtener otros secretos utilizando el Project ID
+    config.WORDPRESS_USERNAME = (await getSecret(projectId, 'wp_username')).trim();
+    config.WORDPRESS_APP_PASSWORD = (await getSecret(projectId, 'wp_app_password')).trim();
+    config.WORDPRESS_API_URL = (await getSecret(projectId, 'WORDPRESS_API_URL')).trim();
     console.log('Credenciales de WordPress obtenidas exitosamente.');
 
-    config.OPENAI_API_KEY = (await getSecret('OPENAI_API_KEY')).trim();
+    config.OPENAI_API_KEY = (await getSecret(projectId, 'OPENAI_API_KEY')).trim();
     console.log('OPENAI_API_KEY obtenido exitosamente.');
 
-    config.SHARED_SECRET = (await getSecret('SHARED_SECRET')).trim();
+    config.SHARED_SECRET = (await getSecret(projectId, 'SHARED_SECRET')).trim();
     console.log('SHARED_SECRET obtenido exitosamente.');
-
-    config.GOOGLE_CLOUD_PROJECT_ID = (await getSecret('GOOGLE_CLOUD_PROJECT_ID')).trim();
-    console.log('GOOGLE_CLOUD_PROJECT_ID obtenido exitosamente.');
 
     // Añade otras configuraciones si es necesario
   } catch (error) {
